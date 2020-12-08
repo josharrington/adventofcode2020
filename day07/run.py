eccb0f6c4d7485249f1ec3e636dfdda51d58875c
Josh Arrington <2245544+josharrington@users.noreply.github.com>
2020-12-07 20:33:09 -0600

    Cleanup Day 7

--- day07/run.py
+++ day07/run.py
@@ -5,18 +5,14 @@ def parse_file(file):
     lines = open(f'{os.path.dirname(__file__)}/{file}', 'r').read().splitlines()
     results = {}
     for line in lines:
-        s = list(map(str.strip, line.split("contain")))
-        key = re.sub('bags?$', '', s[0]).strip()
-        results[key] = {}
-
-        if s[1] != "no other bags.":
-            for item in s[1].split(','):
-                item = re.sub('bags?\.?$', '', item).strip()
-                count, subkey = item.split(' ', 1) 
-                results[key][subkey] = int(count)
+        parent, children = re.match(r'^(.*) bags contain (.*)\.$', line).groups()
+        children = {child[1]: int(child[0]) for child in re.findall('(\d+) (.+?) bag', children)}
+        results[parent] = children
     return results
 
-def part1(tree, find):
+def part1_find_parents(tree, find):
+    # I'm not happy with the O() complexity of this one and I'm sure there's a better
+    # computer sciencey approach. However, for this data set it's quite fast.
     done = False
     bags = set()
     to_find = [find]
@@ -30,15 +26,15 @@ def part1(tree, find):
 
         # If we're done searching
         done = prev_count == len(bags)
-
     return len(bags)
 
 def part2_bags_inside(tree, find):
     # Use recursion to find each child's bag total. Then sum them all up
     total = 0
-    total += sum([tree[find][x] + part2_bags_inside(tree, x)*tree[find][x] for x in tree[find].keys()])
+    total += sum([tree[find][child] + part2_bags_inside(tree, child) * tree[find][child] 
+                  for child in tree[find].keys()])
     return total
 
 tree = parse_file('input.txt')
-print(f"Part 1: {part1(tree, 'shiny gold')}")
+print(f"Part 1: {part1_find_parents(tree, 'shiny gold')}")
 print(f"Part 2: {part2_bags_inside(tree, 'shiny gold')}")
